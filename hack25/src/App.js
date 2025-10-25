11aba982f6b93a67718b82fd1e6086293bc30ddd
marielperezf <a00832811@tec.mx>
2025-10-25 16:41:06 -0600

    Cambios hasta ahora
    Cambios hasta ahora

--- hack25/src/App.js
+++ hack25/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import Home from "./HomePageONG";
+import Home from "./HomePage/HomePageONG";
 import Page1 from "./Page1";
+import InfoNGO from "./InfoNGO/InfoNGO";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/page1" element={<Page1 />} />
+        <Route path="/InfoNGO" element={<InfoNGO />} />
       </Routes>
     </div>
   );
