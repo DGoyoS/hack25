c0e33021de7c32268c4018503301fac5091d32a2
David Sánchez <goyo.sanchez02@gmail.com>
2025-10-25 16:20:32 -0600

    CausesPage almost done

--- hack25/src/App.js
+++ hack25/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./HomePageONG";
 import Page1 from "./Page1";
+import PageCauses from "./PageCauses/PageCauses";
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/page1" element={<Page1 />} />
+        <Route path="/causes" element={<PageCauses />} />
       </Routes>
     </div>
   );
--- hack25/src/HomePageONG.js
+++ hack25/src/HomePageONG.js
@@ -6,8 +6,8 @@ export default function HomePageONG() {
     <div style={{ textAlign: "center", marginTop: "100px" }}>
       <h1>HOLA!</h1>
       <div style={{ display: "flex", justifyContent: "center", gap: "20px", marginTop: "40px" }}>
-        <Link to="/page1"><button>pAG1</button></Link>
-        <Link to="/page2"><button>PAG2</button></Link>
+        <Link to="/page1"><button>Page 1</button></Link>
+        <Link to="/causes"><button>Causes</button></Link>
       </div>
     </div>
   );
